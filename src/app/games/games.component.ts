85465c8ffd74ed0fa3772be1e7a934ee695a15ee
Sir Leo IV <leo-ebeling@web.de>
2023-11-14 22:15:43 +0100

    - make game of life a bit transparent - remember which game was opened

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -7,13 +7,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class GamesComponent implements OnInit {
   gamesOpen: boolean = false;
+  selectedGame: number = 1;
 
   ngOnInit(): void {
     this.gamesOpen = localStorage.getItem("gamesOpen" || "false") == "true";
+    this.selectedGame = parseInt(localStorage.getItem("selectedGame") || "1");
   }
 
   toggleGames() {
     this.gamesOpen = !this.gamesOpen;
     localStorage.setItem("gamesOpen", this.gamesOpen.toString());
+  }
+
+  selectGame(game: number) {
+    this.selectedGame = game;
+    localStorage.setItem("selectedGame", game.toString());
   }
 }
